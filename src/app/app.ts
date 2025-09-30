8424d6c698f85604d81a5602903ef7d9e5e2c472
unknown <temizhangozde@gmail.com>
2025-09-30 11:09:04 +0300

    Add banner component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { Hello } from './hello/hello';
+import { Banner } from './banner/banner';
 
 @Component({
   selector: 'app-root',
-  imports: [Hello],
+  imports: [Hello, Banner],
   templateUrl: './app.html',
   standalone: true,
   styleUrl: './app.scss'
